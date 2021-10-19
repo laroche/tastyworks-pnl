11c811a069ed1ce2a0384eb30482134cb71cea25
Florian La Roche <Florian.LaRoche@gmail.com>
2021-10-19 13:24:28 +0200

    pnl also with 2 digits precision for tax output

--- tw-pnl.py
+++ tw-pnl.py
@@ -764,7 +764,7 @@ def check(wk, output_csv, output_excel, opt_long, verbose, show, debugfifo):
 
         if tax_output:
             if datetime[:4] == tax_output:
-                new_wk.append([datetime[:10], transaction_type(asset_type), local_pnl,
+                new_wk.append([datetime[:10], transaction_type(asset_type), '%.2f' % float(local_pnl),
                     '%.2f' % eur_amount, '%.2f' % (amount - fees), '%.4f' % conv_usd,
                     quantity, asset,
                     '%.2f' % term_loss, tax_free,
